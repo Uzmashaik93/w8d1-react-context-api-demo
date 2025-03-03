b1f84ae530b78b8d3d44d10999ced03ee0339eb7
uzma shaik <uzma97910@gmail.com>
2025-03-03 11:53:18 +0100

    feat:darkand light mode theme

---
+++ src/context/theme.context.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+function ThemeProvider(props) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        {props.children}
+      </ThemeContext.Provider>
+    </>
+  );
+}
+export default ThemeProvider;
+export { ThemeContext };
